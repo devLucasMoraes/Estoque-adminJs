7d56d620d4cb45209faa58bd9957825c36413bff
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-18 18:38:34 -0300

    5: importar resource no server

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import AdminJSExpress from '@adminjs/express'
 import AdminJSSequelize from '@adminjs/sequelize'
 import express from 'express'
 
+import UsersResource from './resources/UsersResources.js'
+
 AdminJS.registerAdapter(AdminJSSequelize)
 
 const app = express()
@@ -13,7 +15,7 @@ const app = express()
 const adminjs = new AdminJS({
     databases: [],
     rootPath: '/admin',
-    resources: []
+    resources: [UsersResource]
 })
 
 const router = AdminJSExpress.buildRouter(adminjs)
