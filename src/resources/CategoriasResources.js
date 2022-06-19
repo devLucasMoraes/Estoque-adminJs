06f857cbd0cbb9a1f0c010c41292cc13698f005f
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-19 14:58:48 -0300

    criado resource

---
+++ src/resources/CategoriasResources.js
@@ -0,0 +1,35 @@
+import Categorias from '../models/categorias.js'
+
+export default {
+    resource: Categorias,
+    options: {
+        parent: {
+            icon: 'Category'
+        },
+        properties: {
+            id: {
+                position: 1
+              },
+              nome: {
+                position: 2,
+                isRequired: true
+              },
+              und_medida: {
+                position: 3,
+                isRequired: true
+              },
+              estoque_min: {
+                position: 4
+              },
+              user_id: {
+                position: 5
+              },
+              createdAt: {
+                position: 6
+              },
+              updatedAt: {
+                position: 7
+              }
+        }
+    }
+}
