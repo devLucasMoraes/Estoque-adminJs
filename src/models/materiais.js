8226a3fb88cbc5a6f17b77fdfea844e2d960f5f3
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-19 17:27:18 -0300

    verificando models

--- src/models/materiais.js
+++ src/models/materiais.js
@@ -1,23 +1,24 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Materiais extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+import Sequelize, { Model } from 'sequelize'
+
+
+class Materiais extends Model {
+  static init(sequelize) {
+    super.init({
+      descricao: Sequelize.STRING
+    }, {
+      sequelize,
+      modelName: 'Materiais',
+    })
   }
-  Materiais.init({
-    descricao: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Materiais',
-  });
-  return Materiais;
-};
+  static associate(models) {
+    this.belongsTo(models.Categorias, {
+      foreignKey: 'categorias_id'
+    })
+    this.belongsTo(models.Users, {
+      foreignKey: 'user_id'
+    })
+  }
+}
+
+export default Materiais
