432176bb56f637118687548dceee04de332967a9
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-24 20:02:19 -0300

    criado metodos de controle de permisao

--- src/services/auth.js
+++ src/services/auth.js
@@ -9,3 +9,12 @@ export const creatPasswordHash = async (password) => {
 export const checkPassword =  (user, password) => {
     return bcrypt.compare(password, user.password_hash)
 }
+
+//// controle de permissoes
+export const hasAdminPermission = (currentUser) => {
+    return currentUser && currentUser.role === "administrador"
+}
+
+export const hasOperatorPermission = (currentUser) => {
+    return currentUser && ['administrador', 'operador'].includes(currentUser.role)
+}
