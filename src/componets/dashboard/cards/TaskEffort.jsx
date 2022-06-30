b53eef1e3a2bb42eb6ba025d679383186ac0cf8a
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-30 19:16:39 -0300

    adicionado modelos do google react charts

--- src/componets/dashboard/cards/TaskEffort.jsx
+++ src/componets/dashboard/cards/TaskEffort.jsx
@@ -1,17 +1,58 @@
 import React from 'react'
 
 import { Text, H5 } from '@adminjs/design-system'
+import { Chart } from 'react-google-charts'
+
 
 import { Card } from '../styles'
+
+const data = [
+    [
+        {
+            type: "date",
+            id: "Date",
+        },
+        {
+            type: "number",
+            id: "Won/Loss",
+        },
+    ],
+    [new Date(2012, 3, 13), 37032],
+    [new Date(2012, 3, 14), 38024],
+    [new Date(2012, 3, 15), 38024],
+    [new Date(2012, 3, 16), 38108],
+    [new Date(2012, 3, 17), 38229],
+    // Many rows omitted for brevity.
+    [new Date(2013, 9, 4), 38177],
+    [new Date(2013, 9, 5), 38705],
+    [new Date(2013, 9, 12), 38210],
+    [new Date(2013, 9, 13), 38029],
+    [new Date(2013, 9, 19), 38823],
+    [new Date(2013, 9, 23), 38345],
+    [new Date(2013, 9, 24), 38436],
+    [new Date(2013, 9, 30), 38447],
+];
 
 const TaskEffort = () => {
     return <Card as="a" href="#">
         <Text textAlign="center">
             <H5>Titulo</H5>
-            <Text>Conteudo</Text>
+            <Text>
+                <Chart
+                    chartType="Calendar"
+                    width="100%"
+                    height="100%"
+                    data={data}
+                />
+            </Text>
         </Text>
     </Card>;
 
 }
 
 export default TaskEffort
+
+
+
+
+
--- src/componets/dashboard/cards/TaskType.jsx
+++ src/componets/dashboard/cards/TaskType.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 
 import { Text, H5 } from '@adminjs/design-system'
+import { Chart } from 'react-google-charts'
+
 
 import { Card } from '../styles'
+
+const data = [
+    ["Task", "Hours per Day"],
+    ["Work", 11],
+    ["Eat", 2],
+    ["Commute", 2],
+    ["Watch TV", 2],
+    ["Sleep", 7],
+];
 
 const TaskType = () => {
     return <Card as="a" href="#">
         <Text textAlign="center">
             <H5>Titulo</H5>
-            <Text>Conteudo</Text>
+            <Text>
+                <Chart
+                    chartType="PieChart"
+                    data={data}
+                    width={"100%"}
+                    height={"100%"}
+                />
+            </Text>
         </Text>
     </Card>;
 
