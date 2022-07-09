b5a212c2dc6bf817bd2c9bb68167745c0ad105b4
devLucasMoraes <devlucasmoraes@gmai.com>
2022-07-09 13:21:58 -0300

    ajsutando seeds

--- src/database/seeders/_/20220624144412-demo-fornecedores.js
+++ src/database/seeders/_/20220624144412-demo-fornecedores.js
@@ -3,11 +3,11 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Fornecedores', [
-      { name: "TOYOINK", fone: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date() },
-      { name: "SUZANO", fone: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date()  },
-      { name: "USUARIO E SEU PODER", fone: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date()  },
-      { name: "VIA BRASIL EDITORA GRAFICA LTDA", fone: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date()  },
-      { name: "ESTOPA ROCHA", fone: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date()  }
+      { name: "TOYOINK", fone1: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date() },
+      { name: "SUZANO", fone1: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date()  },
+      { name: "USUARIO E SEU PODER", fone1: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date()  },
+      { name: "VIA BRASIL EDITORA GRAFICA LTDA", fone1: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date()  },
+      { name: "ESTOPA ROCHA", fone1: "99999999999", user_id: 1, createdAt: new Date(), updatedAt: new Date()  }
     ], {});
   },
 
