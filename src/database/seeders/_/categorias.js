77476b2c7efa41b2d7a06d526ae0b3b6bcd477ec
ltrismegistus <trismegistolucas1@gmail.com>
2022-07-18 17:56:17 -0300

    atualizados seeds de categorias e usuarios

---
+++ src/database/seeders/_/categorias.js
@@ -0,0 +1,36 @@
+const categoriasJSON = [
+{"name":"LIMPADOR DE CHAPA","und_medida":"LT","estoque_min":"20.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"GOMA","und_medida":"LT","estoque_min":"20.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"ENVELOPES","und_medida":"UND","estoque_min":"0.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS PANTONE","und_medida":"KG","estoque_min":"0.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS KEY","und_medida":"KG","estoque_min":"100.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS SENEGAL","und_medida":"KG","estoque_min":"0.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS YELLOW","und_medida":"KG","estoque_min":"100.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS CYANO","und_medida":"KG","estoque_min":"100.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS MAGENTA","und_medida":"KG","estoque_min":"100.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS TRANSFER AMARELO","und_medida":"KG","estoque_min":"20.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS TRANSFER AZUL","und_medida":"KG","estoque_min":"20.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS TRANSFER MAGENTA","und_medida":"KG","estoque_min":"20.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TINTAS TRANSFER PRETO","und_medida":"KG","estoque_min":"20.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"PAPEL","und_medida":"PCT","estoque_min":"0.00","user_id":"1","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"CHAPA F2","und_medida":"UND","estoque_min":"100.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"RESTAURADOR SOLVENTE","und_medida":"LT","estoque_min":"36.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"SOLUCAO FONTE","und_medida":"LT","estoque_min":"20.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"PO GRAFICO","und_medida":"KG","estoque_min":"10.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"VERNIZ","und_medida":"KG","estoque_min":"20.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"BOBINAS","und_medida":"UND","estoque_min":"0.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"REVELADOR","und_medida":"LT","estoque_min":"60.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"�LCOOIS","und_medida":"LT","estoque_min":"200.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-07-02 17:06:36"}
+,
+{"name":"ARAMES","und_medida":"ROLOS","estoque_min":"1.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 21:03:53"},
+{"name":"COLA VERMELHA","und_medida":"KG","estoque_min":"10.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 20:50:14"},
+{"name":"ESTOPA","und_medida":"UND","estoque_min":"10.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"TRAPO","und_medida":"UND","estoque_min":"20.00","user_id":"2","createdAt":"2022-06-30 15:25:20","updatedAt":"2022-06-30 15:25:20"},
+{"name":"FITA ADESIVA","und_medida":"UND","estoque_min":"10.00","user_id":"3","createdAt":"2022-07-08 20:04:59","updatedAt":"2022-07-08 20:04:59"},
+{"name":"FITA DUPLAFACE","und_medida":"UND","estoque_min":"1.00","user_id":"3","createdAt":"2022-07-08 20:05:29","updatedAt":"2022-07-08 20:05:29"}
+]
+
+const array = JSON.parse(JSON.stringify(categoriasJSON))
+//console.log(array)
+
+export default array
